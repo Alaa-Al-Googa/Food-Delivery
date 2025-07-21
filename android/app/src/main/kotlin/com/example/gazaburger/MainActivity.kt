e9da127d88e22428da9c6578b2cb6aea7c082de7
Alaa-Al-Googa <alaaalgooga@gmail.com>
2025-07-21 21:32:31 +0300

    first

---
+++ android/app/src/main/kotlin/com/example/gazaburger/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gazaburger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/router/app_router.dart
@@ -0,0 +1,23 @@
+// import 'package:gazaburger/screen/home.dart';
+// import 'package:gazaburger/screen/login_screen.dart';
+// import 'package:gazaburger/screen/personal_data.dart';
+// import 'package:gazaburger/screen/register_screen.dart';
+// import 'package:gazaburger/screen/splash_screen.dart';
+// import 'package:go_router/go_router.dart';
+
+// final GoRouter appRouter = GoRouter(
+//   initialLocation: '/',
+//   routes: [
+//     GoRoute(path: '/', builder: (context, state) => const SplashScreen()),
+//     GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
+//     GoRoute(
+//       path: '/register',
+//       builder: (context, state) => const RegisterScreen(),
+//     ),
+//     GoRoute(path: '/home', builder: (context, state) => const Home()),
+//     GoRoute(
+//       path: '/personalData',
+//       builder: (context, state) => const PersonalData(),
+//     ),
+//   ],
+// );
